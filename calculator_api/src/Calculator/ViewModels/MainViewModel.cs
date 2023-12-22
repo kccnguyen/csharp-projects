56a730ba1cd3fc4173673bb4397344bd791217f7
kevwinzdesktop <kevcwinz@gmail.com>
2023-12-22 14:01:07 -0600

    Create MainViewModel.cs

---
+++ calculator_api/src/Calculator/ViewModels/MainViewModel.cs
@@ -0,0 +1,35 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using System.Collections.ObjectModel;
+
+
+namespace Calculator.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+
+        public MainViewModel()
+        {
+            HistoryItems = new ObservableCollection<string>();
+        }
+
+        [ObservableProperty]
+        ObservableCollection<string> historyItems;
+
+        [ObservableProperty]
+        string text;
+        readonly string result;
+
+
+        [RelayCommand]
+        void Add()
+        {
+            HistoryItems.Add(Text);
+            Text = string.Empty;
+        }
+
+        async Task Navigate() =>
+               await AppShell.Current.GoToAsync(nameof(HistoryPage));
+    }
+}
