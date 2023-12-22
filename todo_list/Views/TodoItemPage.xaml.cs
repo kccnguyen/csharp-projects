18966f0faa8ac28a0ff1051a4cbd8407d90d96ec
kevwinzdesktop <kevcwinz@gmail.com>
2023-12-22 14:01:34 -0600

    Create TodoItemPage.xaml.cs

---
+++ todo_list/Views/TodoItemPage.xaml.cs
@@ -0,0 +1,35 @@
+using Project5.Data;
+using Project5.Models;
+
+namespace Project5.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TodoItemPage : ContentPage
+    {
+        public TodoItemPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveClicked(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItem)BindingContext;
+            TodoItemDatabase database = await TodoItemDatabase.Instance;
+            await database.SaveItemAsync(todoItem);
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteClicked(object sender, EventArgs e)
+        {
+            var todoItem = (TodoItem)BindingContext;
+            TodoItemDatabase database = await TodoItemDatabase.Instance;
+            await database.DeleteItemAsync(todoItem);
+            await Navigation.PopAsync();
+        }
+
+        async void OnCancelClicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
