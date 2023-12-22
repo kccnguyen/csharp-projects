f28973bfcf5087e573d3f4a736a96b29d73f411c
kevwinzdesktop <kevcwinz@gmail.com>
2023-12-22 14:01:05 -0600

    Create HistoryViewModel.cs

---
+++ calculator_api/src/Calculator/ViewModels/HistoryViewModel.cs
@@ -0,0 +1,20 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+using System.Collections.ObjectModel;
+
+namespace Calculator.ViewModels
+{
+    public partial class HistoryViewModel : ObservableObject
+    {
+        /*string HistoryItems;
+        public HistoryViewModel(MainViewModel HistoryItems)
+        {
+            this.HistoryItems = HistoryItems;
+        }
+
+        [ObservableProperty]
+        string text; */
+
+    }
+}
