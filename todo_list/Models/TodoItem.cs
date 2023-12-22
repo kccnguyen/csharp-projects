dd471c147732bd9bf643239b92b12ebbd0e70b81
kevwinzdesktop <kevcwinz@gmail.com>
2023-12-22 14:01:25 -0600

    Create TodoItem.cs

---
+++ todo_list/Models/TodoItem.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace Project5.Models
+{
+    public class TodoItem
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Task { get; set; }
+        public string Details { get; set; }
+        public bool Done { get; set; }
+    }
+}
+
